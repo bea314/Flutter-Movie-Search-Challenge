e1d484232170b51080aacbc5011e1cfcda36abd4
bea314 <beatrizdelpinal98@gmail.com>
2025-07-19 14:43:28 -0600

    feat: movie response model modification for error responses

--- lib/features/movies/data/models/movie_response_model.dart
+++ lib/features/movies/data/models/movie_response_model.dart
@@ -6,22 +6,26 @@ class MovieResponseModel {
     final List<MovieModel> movies;
     final String totalResults;
     final String response;
+    final String? error; 
 
     MovieResponseModel({
         required this.movies,
         required this.totalResults,
         required this.response,
+        this.error,
     });
 
     MovieResponseModel copyWith({
         List<MovieModel>? movies,
         String? totalResults,
         String? response,
+        String? error,
     }) => 
         MovieResponseModel(
             movies: movies ?? this.movies,
             totalResults: totalResults ?? this.totalResults,
             response: response ?? this.response,
+            error: error ?? this.error,
         );
 
     factory MovieResponseModel.fromJson(String str) => MovieResponseModel.fromMap(json.decode(str));
@@ -32,11 +36,13 @@ class MovieResponseModel {
         movies: List<MovieModel>.from(json["Search"].map((x) => MovieModel.fromMap(x))),
         totalResults: json["totalResults"],
         response: json["Response"],
+        error: json["Error"] as String?, // capture OMDb’s error message
     );
 
     Map<String, dynamic> toMap() => {
         "Search": List<dynamic>.from(movies.map((x) => x.toMap())),
         "totalResults": totalResults,
         "Response": response,
+        if (error != null) "Error": error,
     };
 }
